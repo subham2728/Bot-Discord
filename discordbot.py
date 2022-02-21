9533b76ec040dd3fe19848af02bb7b2d8e258efc
Subham <subham.rouniyar27@gmail.com>
2022-02-21 14:19:15 +0545

    version_1.1.2

--- discordbot.py
+++ discordbot.py
@@ -8,9 +8,8 @@ if __name__ == "__main__":
         
         load_dotenv()
         API_TOKEN = os.environ.get("API_TOKEN")
-        client = discord.Client()
-        bot = commands.Bot(command_prefix='.')
-        
+
+        bot=commands.Bot(command_prefix='.')
         @bot.event
         async def on_ready():
                 print("We have logged in as {0.user}".format(bot))
@@ -19,17 +18,19 @@ if __name__ == "__main__":
         @commands.has_permissions(manage_messages=True)
         async def clear(ctx,amount=None):
                 await ctx.channel.purge(limit=amount)
+             
         
-        @bot.command()
+        @bot.event
         async def on_message(message):
-                if message.author.id==client.user.id:
+                if message.author.id==bot.user.id:
                         return
                 msg_content=message.content.lower()
-                
+
                 links=['.gift']
 
                 if any(word in msg_content for word in links):
                         await message.delete()
                         await message.channel.send("**No nitro scem allowed in this channel**")
+
                 
 bot.run(API_TOKEN)
